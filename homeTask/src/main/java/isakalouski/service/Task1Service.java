f634afdc0d40d9bf6b66735dca12e74cbb2dffd2
ilya-sakalouski <ilya6697089@mail.ru>
2019-12-15 16:40:26 +0300

    TASK1 & TASK2

--- homeTask/src/main/java/isakalouski/service/Task1Service.java
+++ homeTask/src/main/java/isakalouski/service/Task1Service.java
@@ -1,5 +1,7 @@
 package isakalouski.service;
 
 public interface Task1Service {
+
     void runTask1();
+
 }
--- homeTask/src/main/java/isakalouski/service/Task2Service.java
+++ homeTask/src/main/java/isakalouski/service/Task2Service.java
@@ -1,5 +1,7 @@
 package isakalouski.service;
 
 public interface Task2Service {
+
     void runTask2();
+
 }
